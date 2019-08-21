d13e5852bbf4f03d2c2dfa134ab8e4c3719545ed
Ricardo Lopes <ricardo.sao@hotmail.com>
2019-08-21 10:55:12 -0300

    Criação do projeto e inserção dos códigos para retornar dados da máquina

---
+++ teste/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[]
+            {
+               "wmic cpu get name",
+               "wmic memorychip get capacity",
+               "wmic diskdrive get mediatype",
+               "wmic diskdrive get size",
+               "wmic diskdrive get status",
+               "wmic bios get serialnumber",
+               "wmic csproduct get name",
+               "wmic csproduct get vendor",
+            };
+            //Instancia o cmd
+            Process cmd = new Process();
+            //Abre o cmd
+            cmd.StartInfo.FileName = "cmd.exe";            
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            //Cria a janela
+            cmd.StartInfo.CreateNoWindow = true;
+            //Não esecuta no power shell
+            cmd.StartInfo.UseShellExecute = false;            
+            cmd.Start();
+            //Executa o comando dentro do cmd aberto
+            foreach(string comando in array)
+            {
+                cmd.StandardInput.WriteLine(comando);
+            }
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            string teste = cmd.StandardInput.ToString();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            //Necessário para o cmd não fechar após executar a requisição            
+            Console.ReadKey();
+        }
+    }
+}
